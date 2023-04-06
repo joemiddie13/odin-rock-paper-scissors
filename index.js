273cadf299deeb076a902178d06a37e10ba7e526
joemiddie13 <joemiddie13@gmail.com>
2023-04-06 16:58:46 -0700

    Creating my first JavaScript Function
    Naming the function "getComputerChoice".
    Creating an array of options [Rock, Paper, Scissors].
    Currently researching different types of ways to create the output.

---
+++ index.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    var options = ["Rock", "Paper", "Scissors"];
+    var randomIndex = Math, floor(Math.random() * options.length);
+    return options[randomIndex];
+}
