9a74746e63947dc2da4398790d5dc3f317c1f2e2
joemiddie13 <joemiddie13@gmail.com>
2023-04-07 16:51:15 -0700

    Creating Loop for Multiple Rounds
    We need to create a loop that will play 5 Rounds
    of rock, paper, scissors. Creating two variables, 'playerWins',
    and 'computerWins', we set them to 0 for the start.
    
    I'm understanding the concept of loops but still working
    on full wrapping my mind around on how to create one
    in different scenarios. Loops are such a cool but simple concept.
    
    Next, working on the console will prompt the user
    for their choice and how it will print to the console.

--- index.js
+++ index.js
@@ -7,25 +7,56 @@ function getComputerChoice() {
 // const hand = getComputerChoice();
 // console.log(hand);
 
-function playRound(playerSelection, computerSelection) {
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = playerSelection.toLowerCase();
+//     if (playerSelection === computerSelection) {
+//         return "Tie! Both players chose " + playerSelection + ".";
+//     } else if (playerSelection == "rock" && computerSelection == "paper") {
+//         return "You lose! " + "Computer chose " + computerSelection + "!";
+//     } else if (playerSelection == "rock" && computerSelection == "scissors") {
+//         return "You win! The computer chose " + computerSelection + ".";
+//     } else if (playerSelection == "paper" && computerSelection == "rock") {
+//         return "You win! The computer chose " + computerSelection + ".";
+//     } else if (playerSelection == "paper" && computerSelection == "scissors") {
+//         return "You lose! The computer chose " + computerSelection + ".";
+//     } else if (playerSelection == "scissors" && computerSelection == "rock") {
+//         return "You lose! The computer chose " + computerSelection + ".";
+//     } else if (playerSelection == "scissors" && computerSelection == "paper") {
+//         return "You win! The computer chose " + computerSelection + ".";
+//     }
+// }
+
+// const playerSelection = "ROCK";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+function playRound(playerSelection, getComputerChoice) {
     playerSelection = playerSelection.toLowerCase();
-    if (playerSelection === computerSelection) {
-        return "Tie! Both players chose " + playerSelection + ".";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lose! " + "Computer chose " + computerSelection + "!";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! The computer chose " + computerSelection + ".";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win! The computer chose " + computerSelection + ".";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose! The computer chose " + computerSelection + ".";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lose! The computer chose " + computerSelection + ".";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! The computer chose " + computerSelection + ".";
+    if (playerSelection === getComputerChoice) {
+        return "You draw!";
+    } else if (playerSelection == "rock" && getComputerChoice == "scissors") {
+        return "You win!";
+    } else if (playerSelection == "paper" && getComputerChoice == "rock") {
+        return "You win!";
+    } else if (playerSelection == "scissors" && getComputerChoice == "paper") {
+        return "You win!";
+    } else {
+        return "You lose!";
     }
 }
 
-const playerSelection = "ROCK";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (var i = 0; i < 5; i++) {
+        let playerSelection = prompt("Make your choice: rock, paper, or scissors?");
+        let computerSelection = getComputerChoice
+        let result = playRound(playerSelection, computerSelection);
+        console.log("Round " + (i + 1) + ": You chose " + playerSelection + computerSelection + ", result: " + result);
+        if (result === "You win!") {
+            playerWins++;
+        } else if (result === "You lose!") {
+            computerWins++;
+        }
+    }
+}
