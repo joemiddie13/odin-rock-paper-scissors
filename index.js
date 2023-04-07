83eafe21372a9c9ffdbfd838f8015608789f429d
joemiddie13 <joemiddie13@gmail.com>
2023-04-06 23:33:53 -0700

    Player Choice & Computer Choice Function
    Created a 'playRound' function with two parameters to
    represent the player selection and the computer selection.
    
    Made a bunch of 'if' statements for all the possible
    match-ups.
    
    Result prints properly in the console based on the
    pre-determined player choice of "rock". Need to figure out
    how to ask for a player input, next.

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ function getComputerChoice() {
     return options[randomChoice];
   }
 
-const hand = getComputerChoice();
-console.log(hand);
+// const hand = getComputerChoice();
+// console.log(hand);
 
 function playRound(playerSelection, computerSelection) {
-
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "Tie! Both players chose " + playerSelection + ".";
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lose! " + "Computer chose " + computerSelection + "!";
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You win! The computer chose " + computerSelection + ".";
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You win! The computer chose " + computerSelection + ".";
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You lose! The computer chose " + computerSelection + ".";
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lose! The computer chose " + computerSelection + ".";
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You win! The computer chose " + computerSelection + ".";
+    }
 }
+
+const playerSelection = "ROCK";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
