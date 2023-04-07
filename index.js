6b89076111ba691d0cf0eee39a4f49d1b2c903ee
joemiddie13 <joemiddie13@gmail.com>
2023-04-06 19:39:02 -0700

    Created a function named 'getComputerChoice'.
    Created an array, 'const options', with the choices of "rock, paper,
    and scissors".
    
    Created a variable, 'const randomChoice', that will generate a random
    number between 0 and 1, round it to the nearest integer, and assign it
    to one of the options in the array.
    
    Currently working on creating a function so the I can play against
    the computer.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * options.length);
-    return options[randomIndex];
+    const randomChoice = Math.floor(Math.random() * options.length);
+    return options[randomChoice];
   }
 
 const hand = getComputerChoice();
 console.log(hand);
 
 function playRound(playerSelection, computerSelection) {
-    
+
 }
