95d37b2a717584f55197efbc90450555420dedf0
joemiddie13 <joemiddie13@gmail.com>
2023-04-07 19:05:52 -0700

    We have... ROCK, PAPER, SCISSORS!
    I had to re-write many parts to my code. For example,
    in the 'playRound' function I used 'const' for the 'playerSelection'
    parameter. Const was not necessary as it is already
    a function.
    
    Originally I had 'compChoice' variable for 'computerSelection'
    'compChoice' was set only once before the game started
    so it would not change during the game. Instead I called
    'getComputerChoice()' function inside the loop. This would
    allow me to get a new computer choice after each round.
    
    Will pause on this code until the upcoming lessons to build the GUI.

--- index.js
+++ index.js
@@ -3,71 +3,43 @@ function getComputerChoice() {
     const randomChoice = Math.floor(Math.random() * options.length);
     return options[randomChoice];
   }
-
-// const hand = getComputerChoice();
-// console.log(hand);
-
-// function playRound(playerSelection, computerSelection) {
-//     playerSelection = playerSelection.toLowerCase();
-//     if (playerSelection === computerSelection) {
-//         return "Tie! Both players chose " + playerSelection + ".";
-//     } else if (playerSelection == "rock" && computerSelection == "paper") {
-//         return "You lose! " + "Computer chose " + computerSelection + "!";
-//     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-//         return "You win! The computer chose " + computerSelection + ".";
-//     } else if (playerSelection == "paper" && computerSelection == "rock") {
-//         return "You win! The computer chose " + computerSelection + ".";
-//     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-//         return "You lose! The computer chose " + computerSelection + ".";
-//     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-//         return "You lose! The computer chose " + computerSelection + ".";
-//     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-//         return "You win! The computer chose " + computerSelection + ".";
-//     }
-// }
-
-// const playerSelection = "ROCK";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
-
-
-const compChoice = getComputerChoice()
-
-function playRound(playerSelection, compChoice) {
+  
+  function playRound(playerSelection, compChoice) {
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === compChoice) {
-        return "You draw!";
+      return "You draw!";
     } else if (playerSelection == "rock" && compChoice == "scissors") {
-        return "You win!";
+      return "You win!";
     } else if (playerSelection == "paper" && compChoice == "rock") {
-        return "You win!";
+      return "You win!";
     } else if (playerSelection == "scissors" && compChoice == "paper") {
-        return "You win!";
+      return "You win!";
     } else {
-        return "You lose!";
+      return "You lose!";
     }
-}
-
-function game() {
+  }
+  
+  function game() {
     let playerWins = 0;
     let computerWins = 0;
     for (var i = 0; i < 5; i++) {
-        playerSelection = prompt("Make your choice: rock, paper, or scissors?");
-        // computerSelection = compChoice
-        let result = playRound(playerSelection, compChoice);
-        console.log("Round " + (i + 1) + ": You chose " + playerSelection + compChoice + ", result: " + result);
-        if (result === "You win!") {
-            playerWins++;
-        } else if (result === "You lose!") {
-            computerWins++;
-        }
+      playerSelection = prompt("Make your choice: rock, paper, or scissors?");
+      computerSelection = getComputerChoice();
+      let result = playRound(playerSelection, computerSelection);
+      console.log(
+        "Round " + (i + 1) + ": You chose " + playerSelection + ", computer chose " + computerSelection + ", result: " + result);
+      if (result === "You win!") {
+        playerWins++;
+      } else if (result === "You lose!") {
+        computerWins++;
+      }
     }
     console.log("Final score: You " + playerWins + ", computer " + computerWins);
     if (playerWins > computerWins) {
-        console.log("You win the game!");
+      console.log("You win the game!");
     } else if (playerWins < computerWins) {
-        console.log("You lose the game.");
+      console.log("You lose the game.");
     } else {
-        console.log("The game is a draw.");
+      console.log("The game is a draw.");
     }
-}
+  }
